c2ab242db78929c6cf1bf672906c97a2a3454ce5
Leandro Silva <lansoweb@hotmail.com>
2015-01-18 16:41:12 -0200

    Returning AbstractModule

---
+++ src/LosBase/Module/AbstractModule.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Definição de uma classe abstrata para os módulos LosXX
+ *
+ * @package   LosBase\Module
+ * @author    Leandro Silva <leandro@leandrosilva.info>
+ * @link      http://leandrosilva.info Development Blog
+ * @link      http://github.com/LansoWeb/LosBase for the canonical source repository
+ * @copyright Copyright (c) 2011-2015 Leandro Silva (http://leandrosilva.info)
+ * @license   http://leandrosilva.info/licenca-bsd New BSD license
+ */
+namespace LosBase\Module;
+
+use InvalidArgumentException;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\LocatorRegisteredInterface;
+
+/**
+ * Definição de uma classe abstrata para os módulos LosXX
+ *
+ * @package     LosBase\Module
+ * @author      Leandro Silva <leandro@leandrosilva.info>
+ * @link        http://leandrosilva.info Development Blog
+ * @link        http://github.com/LansoWeb/LosBase for the canonical source repository
+ * @copyright   Copyright (c) 2011-2015 Leandro Silva (http://leandrosilva.info)
+ * @license     http://leandrosilva.info/licenca-bsd New BSD license
+ */
+abstract class AbstractModule implements AutoloaderProviderInterface, LocatorRegisteredInterface
+{
+
+    /**
+     * Retorna o diretório atual
+     */
+    private function getDir()
+    {
+        $reflector = new \ReflectionClass(get_class($this));
+        $filename = $reflector->getFileName();
+
+        return dirname($filename);
+    }
+
+    /**
+     * Retorna o namespace atual
+     */
+    private function getNamespace()
+    {
+        $reflector = new \ReflectionClass(get_class($this));
+
+        return $reflector->getNamespaceName();
+    }
+
+    /**
+     * Configura os Autolodaers
+     *
+     * @see \Zend\ModuleManager\Feature\AutoloaderProviderInterface::getAutoloaderConfig()
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                $this->getDir() . '/../../autoload_classmap.php'
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    $this->getNamespace() => $this->getDir() . '/../../src/' . $this->getNamespace()
+                )
+            )
+        );
+    }
+
+    public function getConfig()
+    {
+        return include $this->getDir() . '/../../config/module.config.php';
+    }
+}
