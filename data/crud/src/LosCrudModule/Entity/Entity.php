759d0d45f6caf10721d594ba883cb71b84707d1d
Leandro Silva <leandro@leandrosilva.info>
2015-02-01 07:16:33 -0200

    Changing annotation label to be standard with the rest of the module

--- data/crud/src/LosCrudModule/Entity/Entity.php
+++ data/crud/src/LosCrudModule/Entity/Entity.php
@@ -2,15 +2,15 @@
 namespace __MODULENAME__\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Zend\Form\Annotation;
+use Zend\Form\Annotation as Form;
 use LosBase\Entity\AbstractEntity;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="__MODULEDASHEDNAME__")
- * @Annotation\Name("form__MODULENAME__")
- * @Annotation\Hydrator("Zend\Stdlib\Hydrator\ObjectProperty")
- * @Annotation\Type("LosBase\Form\AbstractForm")
+ * @Form\Name("form__MODULENAME__")
+ * @Form\Hydrator("Zend\Stdlib\Hydrator\ObjectProperty")
+ * @Form\Type("LosBase\Form\AbstractForm")
  */
 class __MODULENAME__ extends AbstractEntity
 {
